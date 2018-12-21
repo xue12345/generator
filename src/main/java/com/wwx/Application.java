5c6ea7e4003c0ac6a988d08824bf8c565c0fa2bf
wangxue <936878487@qq.com>
2018-12-21 10:18:32 +0800

    自动代码生成第一版

---
+++ src/main/java/com/wwx/Application.java
@@ -0,0 +1,14 @@
+package com.wwx;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.wwx.dao")
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
